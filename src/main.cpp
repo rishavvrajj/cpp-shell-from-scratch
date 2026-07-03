73ecf58f61091a5c5d1cb839a229abcf3aedf492
rishav raj <rishav5raj3@gmail.com>
2026-07-04 02:01:41 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@ int main() {
     cout << "$ ";
     string command;
     cin >> command;
+    if (command == 'exit') {
+      return;
+    };
     cout << command << ": command not found" << endl;
   }
 }
